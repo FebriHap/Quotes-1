5038336b71037f835b0ccae295d0b788e0b634ec
putrihsnh <69383799+DiasKreizt@users.noreply.github.com>
2022-12-05 10:41:20 +0700

    adas

--- app/src/main/java/com/ahmfarisi/quotes/Activity/MainActivity.java
+++ app/src/main/java/com/ahmfarisi/quotes/Activity/MainActivity.java
@@ -7,10 +7,15 @@ import android.os.Bundle;
 import com.ahmfarisi.quotes.R;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvQuotes;
+    private ProgresBar pbQuotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvQuotes = findViewBiId(R.id.rv_Quotes);
+        pbQuotes = fin
     }
 }
